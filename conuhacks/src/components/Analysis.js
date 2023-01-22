6a9d04d2cbcd80529a7f6e5f0cd3d50b37f88d95
justpino7 <nicopino_7@hotmail.com>
2023-01-22 06:25:29 -0500

    Reddit thread css part

--- conuhacks/src/components/Analysis.js
+++ conuhacks/src/components/Analysis.js
@@ -1,19 +1,44 @@
 import React from "react";
-import "./Analysis.css"
+import "./Analysis.css";
+import UFC from './UFC.png';
+
 
 const Analysis = () => {
     return(
         <div className="analysis-container">
             <h1 className="title">
-                Sentimental Analysis of a UFC Reddit Thread 
+                Sentimental Analysis of a UFC Reddit Thread
             </h1>
-            <div className="description">
-                An analysis of user comments on a UFC reddit thread 
-                before the event and after. 
-                Comments from 2 reddit thread are analysed using 
-                sentiment NLP to determine if they are mostly positive 
-                or negative. A word cloud is also used to demonstrate the 
-                most commonly used words in the reddit thread. 
+            <div className="main-container">
+                <div className="reddit-item">
+                    An analysis of user comments on a UFC reddit thread 
+                    before the event and after. 
+                    Comments from 2 reddit thread are analysed using 
+                    sentiment NLP to determine if they are mostly positive 
+                    or negative. A word cloud is also used to demonstrate the 
+                    most commonly used words in the reddit thread. 
+                    <br>
+                    </br>
+                    <br>
+                    </br>
+                    <h2>
+                        First Thread: <br>
+                        </br>[Official] UFC 283: Teixeira vs. Hill Weigh-In & Pre-Fight Discussion Thread
+                    </h2>
+                    <br>
+                    </br>
+                    <br>
+                    </br>
+                    <h2>
+                        Second Thread: <br>
+                        </br>[Official] UFC 283: Teixeira vs. Hill Weigh-In & Post-Fight Discussion Thread
+                    </h2>
+                </div>
+                <div className="reddit-item">
+                    <div className="poster-img-div">
+                        <img className="poster-img" src={UFC} alt="UFC" />
+                    </div>
+                </div>
             </div>
         </div>
     )
