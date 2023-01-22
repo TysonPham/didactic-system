2141c653e6329b21e52422253468436e8ffcec68
justpino7 <nicopino_7@hotmail.com>
2023-01-22 08:13:33 -0500

    Forms css for the transition bar

--- conuhacks/src/components/Forms.js
+++ conuhacks/src/components/Forms.js
@@ -7,26 +7,37 @@ const its = nlp.its;
 const as = nlp.as;
 const Forms = () => {
 
-    const form = useRef();
-    const[relationship1Score, SetRelationship1Score] = React.useState(0);
-    const[relationship2Score, SetRelationship2Score] = React.useState(0);
-    const[perception1Score, SetPerception1Score] = React.useState(0);
-    const[perception2Score, SetPerception2Score] = React.useState(0);
-    const[totalScore, SetTotalScore] = React.useState(0);
+    const [relationship1Score, SetRelationship1Score] = React.useState(0);
+    const [relationship2Score, SetRelationship2Score] = React.useState(0);
+    const [perception1Score, SetPerception1Score] = React.useState(0);
+    const [perception2Score, SetPerception2Score] = React.useState(0);
+    const [totalScore, SetTotalScore] = React.useState(0);
+    const [style, setStyle] = React.useState({});
+    const [done, setDone] = React.useState(0);
+    var element = document.getElementById('btn');
 
-    const sendEmail = (e) => {
-    e.preventDefault();
+    setTimeout(() => {
+        const newStyle = {
+            opacity: 1,
+            width: `${done}%`
+        }
 
-    const relationship1 =document.getElementById("answer1").value;
-    const relationship2 =document.getElementById("answer2").value;
-    const perception1 =document.getElementById("answer3").value;
-    const perception2 =document.getElementById("answer4").value;
+        setStyle(newStyle);
+    }, 200);
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        const relationship1 = document.getElementById("answer1").value;
+        const relationship2 = document.getElementById("answer2").value;
+        const perception1 = document.getElementById("answer3").value;
+        const perception2 = document.getElementById("answer4").value;
 
 
 
         //user input
         //return int -1 to 1
-        let sentiment = (input)=>{
+        let sentiment = (input) => {
             const text = input;
             const doc = nlp.readDoc(text);
             return (doc.out(its.sentiment));
@@ -35,11 +46,16 @@ const Forms = () => {
 
         SetRelationship1Score(sentiment(relationship1));
         SetRelationship2Score(sentiment(relationship2));
-        SetPerception1Score(sentiment(perception1));
-        SetPerception2Score(sentiment(perception2));
-        SetTotalScore((sentiment(relationship1) + sentiment(relationship2) + sentiment(perception1) + sentiment(perception2)) /5);
+        SetPerception1Score(sentiment(perception1) + 1);
+        SetPerception2Score(sentiment(perception2) + 1);
+        SetTotalScore((sentiment(relationship1) + sentiment(relationship2) + sentiment(perception1) + sentiment(perception2)) / 4);
 
+        setDone(Math.round((sentiment(relationship1) + 1) * 50));
 
+        element.classList.remove("timelapse");
+        setTimeout(function () {
+            element.addClass('timelapse');
+        }, 100)
 
     };
 
@@ -50,44 +66,56 @@ const Forms = () => {
 
             <div className="contact__container container grid">
                 <div className="contact__content">
-                        <h3 className="contact__title">Relationships</h3>
+                    <h3 className="contact__title">Relationships</h3>
 
-                        <form ref={form} onSubmit={sendEmail} className="contact__form">
-                            <div className="contact__form-div contact__form-area">
-                                <textarea id="answer1" name="project" cols="30" rows="10"
+                    <form onSubmit={submitHandler} className="contact__form">
+                        <div className="contact__form-div contact__form-area">
+                            <textarea id="answer1" name="project" cols="30" rows="10"
                                 placeholder="Describe relationship with coworkers..."
                                 className="contact__form-input"></textarea>
-                            </div>
+                        </div>
 
-                            <div className="contact__form-div contact__form-area">
-                                <textarea id="answer2" name="project" cols="30" rows="10"
+                        <div className="contact__form-div contact__form-area">
+                            <textarea id="answer2" name="project" cols="30" rows="10"
                                 placeholder="Describe relationship with Mentor/Supervisor..."
                                 className="contact__form-input"></textarea>
-                            </div>
-                        </form>
+                        </div>
+                    </form>
                 </div>
 
                 <div className="contact__content">
-                        <h3 className="contact__title">Perceptions</h3>
+                    <h3 className="contact__title">Perceptions</h3>
 
-                        <form ref={form} onSubmit={sendEmail} className="contact__form">
-                            <div className="contact__form-div contact__form-area">
-                                <textarea id="answer3" name="project" cols="30" rows="10"
+                    <form onSubmit={submitHandler} className="contact__form">
+                        <div className="contact__form-div contact__form-area">
+                            <textarea id="answer3" name="project" cols="30" rows="10"
                                 placeholder="Describe how you percieve your current tasks and workload..."
                                 className="contact__form-input"></textarea>
-                            </div>
-                            <div className="contact__form-div contact__form-area">
-                                <textarea id="answer4" name="project" cols="30" rows="10"
+                        </div>
+                        <div className="contact__form-div contact__form-area">
+                            <textarea id="answer4" name="project" cols="30" rows="10"
                                 placeholder="Describe how you percieve your workspace..."
                                 className="contact__form-input"></textarea>
-                            </div>
-                            <div className='button__middle'>
-                                <button className="button button--flex">Submit</button>
-                            </div>
-                        </form> 
+                        </div>
+
+                    </form>
                 </div>
-                `<div> {relationship1Score} {relationship2Score} {perception1Score} {perception2Score} {totalScore}</div>   `
+
+
             </div>
+            <div className='button__middle'>
+                <button className="button button--flex" onClick={submitHandler}>Submit</button>
+            </div>
+            <div className='good__container'>
+                <div className="progress stack-over-top">
+                    <div className="progress-done " id='btn' style={style}>{done}%</div>
+                </div>
+                <div className="progress">
+                    <div className="progress-bg"></div>
+
+                </div>
+            </div>
+
         </section>
     )
 }
