4658a90459f597c55dad35f5dae8f19b673b0cad
Jeremy Vo Buu <vobuujeremy@hotmail.com>
2023-01-21 22:38:48 -0500

    Added title to welcome page

---
+++ conuhacks/src/components/Welcome.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Welcome.css"
+
+const Welcome = () => {
+
+    return (
+    <div className="container">
+        <h1 className= "title">Welcome to the GSoft Team</h1>
+        <div className="">
+            We are excited to have onboard the team. We hope you are ready meet a wonderful team and face new challenges.
+        </div>
+        <div className="office-container">
+            <div className="office-text">
+                <h2 className="office-title">
+                    Campus life
+                </h2>
+            </div>
+            <div className="office-pic">
+
+            </div>
+        </div>
+    </div>
+    )
+}
+
+export default Welcome;
